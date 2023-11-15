aac1f7a1321685629e23c6d72feb341b00b5ba6b
kevin4052 <kevin.hernandez@gmail.com>
2023-11-14 20:29:50 -0600

    pokecard component

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,34 @@
+import { ReactComponentElement } from 'react';
 import PokeCard from '../components/PokeCard'
 
+interface PokemonItem {
+  name: string,
+  url: string,
+}
+
+async function getPokemonList() {
+  const responce = await fetch('https://pokeapi.co/api/v2/pokemon?limit=150');
+  const data = await responce.json();
+
+  const list: PokemonItem[] = data.results.map((item: { name: string; url: string}) => {
+    return {
+      name: item.name,
+      url: item.url
+    }
+  });
+
+  return list
+}
+
 export default async function Home() {
+  const pokemons = await getPokemonList();
+  const pokeCards = pokemons.map((pokemon: PokemonItem, i) => {
+        return <PokeCard key={i + pokemon.name} pokemon={pokemon} id={i + 1} isPriority={i < 10}></PokeCard>
+      });
+
   return (
-    <div className="container p-4">
-      <PokeCard pokemonId={1}></PokeCard>
+    <div className="container py-4 px-8 m-auto flex flex-row flex-wrap justify-center">
+      {pokeCards}
     </div>
   )
 }
--- components/PokeCard.tsx
+++ components/PokeCard.tsx
@@ -11,6 +11,11 @@ interface PokemonTypes {
   name: string; 
 }
 
+interface PokemonItem {
+  name: string,
+  url: string,
+}
+
 async function getPokemon(id: number) {  
   const data = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}/`, { cache: 'force-cache' });
   const responce = await data.json();
@@ -22,35 +27,26 @@ async function getPokemon(id: number) {
     }
   })
 
+  //console.log(responce.sprites.other['official-artwork'])
+
   const pokemon: Pokemon = {
     name: responce.name,
     types: types,
-    image: responce.sprites.other.home.front_default
+    image: responce.sprites.other["official-artwork"]?.front_default
   }
 
   return pokemon;
 }
 
-export default async function PokeCard(props: {pokemonId: number}) {
-  const { pokemonId } = props;
-  const pokemon = await getPokemon(pokemonId);
+export default async function PokeCard(props: {pokemon: PokemonItem, id: number, isPriority: boolean}) {
+  const { pokemon, id, isPriority } = props;
+  // const pokemon = await getPokemon(pokemonId);
 
   return (
     <div className='m-2 p-4 bg-white text-white rounded-3xl w-80' style={{background: "#46d1b1"}}>
-      <div className='text-lg font-extrabold'>{pokemon.name}</div>
-      <div className='flex justify-between'>
-        <div>
-          <ul>
-            {pokemon.types.map(type => {
-              return (
-                <li className='rounded-xl bg-cyan-500 px-2 my-1 align-middle' style={{background: "#61e0ca"}} key={type.slot}>{type.name}</li>
-              )
-            })}
-          </ul>
-        </div>
-        <div>
-          <Image src={pokemon.image} width={150} height={150} alt={pokemon.name} />
-        </div>
+      <div className='text-2xl font-extrabold px-4'>{pokemon.name}</div>
+      <div className='flex justify-end'>
+          <Image priority={isPriority} src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${id}.png`} width={150} height={150} alt={pokemon.name} />
       </div>
     </div>
   )
